09d903cf3369a7325badf8af266e6d12320c836a
lcesaro-adh <ludovico.cesaro@extern-azdigital-health.com>
2022-04-21 14:14:32 +0200

    removed unnecessary files

--- playground.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-data = [['tom', 10], ['nick', 15], ['juli', 14]]
-# Create the pandas DataFrame
-input_df = pd.DataFrame(data, columns = ['Name', 'Age'])
-
-combination_columns = [col for col in input_df.columns]
-print(combination_columns)
