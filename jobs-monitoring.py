1cded232be2b03c8b52bbec2431b9c29c1bbfc47
lcesaro-adh <ludovico.cesaro@extern-azdigital-health.com>
2022-04-21 09:27:02 +0200

    recurrent scheduling

--- jobs-monitoring.py
+++ jobs-monitoring.py
@@ -20,7 +20,7 @@ response = requests.get(
 webhook = os.getenv("WEBHOOK")
 
 
-def job(threshold, response):
+def job():
     # Get specifics durations from response and converting into json readable
     response_json = response.json()
     # Getting duration and thresholds and Appids
@@ -52,19 +52,19 @@ def job(threshold, response):
                 )
                 message = " ".join(map(str, result))
                 print(message)
-                #notify_slack(message)
+                # notify_slack(message)
         else:
-            message='Error: App ids of the thresholds are not matching with the history server'
+            message = "Error: App ids of the thresholds are not matching with the history server ones"
             print(message)
-            #notify_slack(message)
+            # notify_slack(message)
     except Exception as e:
         print(e)
-        #notify_slack(e)
+        # notify_slack(e)
 
 
 # Send notification to slack channel
 def notify_slack(message):
-    payload = {"text":message}
+    payload = {"text": message}
     r = requests.post(webhook, data=json.dumps(payload))
     print(r.text)
 
@@ -83,10 +83,14 @@ def getDuration(array, attempt):
     return array
 
 
-job(threshold, response)
 # Scheduling the job every 2 weeks
-# schedule.every(2).weeks.at("10:00").do(job)
+schedule.every(14).days.at("10:30").do(job)
 
-# while True:
-#     schedule.run_pending()
-#     time.sleep(1)
+# testing purposes
+#schedule.every(10).seconds.do(job)
+#job(threshold, response)
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
