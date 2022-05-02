c31788bc4f9c07dc64a495d763d8a0f60193dd78
lcesaro-adh <ludovico.cesaro@extern-azdigital-health.com>
2022-05-02 19:35:10 +0300

    setup virtualenv to be fixed,dynamic index and person working properly

--- new_data_generation.py
+++ new_data_generation.py
@@ -5,6 +5,13 @@ import sys
 
 pd.set_option("display.max_columns", None)
 d = dict(os.environ)
+# # SETUP Virtual Environment
+# py_env = subprocess.check_call([sys.executable,"-m","pyenv", "activate", "dp"])
+# py_path = subprocess.check_call([sys.executable,"-m","export", "PYTHONPATH=$PWD:$PYTHONPATH"])
+# py_path2 = subprocess.check_call([sys.executable,"-m","echo", "$PYTHONPATH"])
+# #env_req = subprocess.run(["pip", "install", "-r", "/Users/ludovicocesaro/Desktop/Files/Reply/Allianz/Projects/datahub-pipelines/requirements.txt"], shell=True, capture_output=True)
+# env_req = subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "/Users/ludovicocesaro/Desktop/Files/Reply/Allianz/Projects/datahub-pipelines/requirements.txt"])
+# print(py_env, py_path,py_path2, env_req,'status setup')
 
 def generate_anonymized_data():
     """
@@ -15,28 +22,20 @@ def generate_anonymized_data():
     times = int(input("Please enter how many times you want to enlarge the datasets:\n"))
     print(f"You choosed to enlarge by {times} times")
 
-    # SETUP Virtual Environment
-    py_env = subprocess.check_call([sys.executable,"-m","pyenv", "activate", "dp"])
-    py_path = subprocess.check_call([sys.executable,"-m","export", "PYTHONPATH=$PWD:$PYTHONPATH"])
-    py_path2 = subprocess.check_call([sys.executable,"-m","echo", "$PYTHONPATH"])
-    #env_req = subprocess.run(["pip", "install", "-r", "/Users/ludovicocesaro/Desktop/Files/Reply/Allianz/Projects/datahub-pipelines/requirements.txt"], shell=True, capture_output=True)
-    env_req = subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "/Users/ludovicocesaro/Desktop/Files/Reply/Allianz/Projects/datahub-pipelines/requirements.txt"])
-    print(py_env, py_path,py_path2, env_req,'status setup')
-
     for x in range(times):
         print('Anonymization run', x+1)
         # RUN ANONYMIZATION SCRIPT
-        #TODO: FIX python_env & env_var / make work command
-        next = x+1
-        anonymize = subprocess.run(
-            [
-                f"python",
-                "/Users/ludovicocesaro/Desktop/Files/Reply/Allianz/Projects/datahub-pipelines/tasks/common/anonymization/secure_anonymize.py",
-                "/Users/ludovicocesaro/Downloads/test/{}".format(x),
-                "/Users/ludovicocesaro/Downloads/test/{}".format(next)
-            ])
-        # command for anonymization
-        # TODO: path must be generalized for working in all environments
+        # #TODO: FIX python_env & env_var / make work command
+        # next = x+1
+        # anonymize = subprocess.run(
+        #     [
+        #         f"python",
+        #         "/Users/ludovicocesaro/Desktop/Files/Reply/Allianz/Projects/datahub-pipelines/tasks/common/anonymization/secure_anonymize.py",
+        #         "/Users/ludovicocesaro/Downloads/test/{}".format(x),
+        #         "/Users/ludovicocesaro/Downloads/test/{}".format(next)
+        #     ])
+        # # command for anonymization
+        # # TODO: path must be generalized for working in all environments
 
     combine(times)
 
@@ -51,6 +50,8 @@ def combine(times):
 
         old_claims = pd.read_csv("/Users/ludovicocesaro/Downloads/test/{}/claims.csv".format(times-1))
         new_claims = pd.read_csv("/Users/ludovicocesaro/Downloads/test/{}/claims.csv".format(times))
+
+        index = 281533 # Base index
 
         # _2 Containing ALL (new and old concatenated)
         po_frames = [old_policies, new_policies]
@@ -68,12 +69,12 @@ def combine(times):
         # claims_2.drop('Unnamed: 0', axis=1, inplace=True)
 
         # Taking random ids from persons to make them match
-        random_persons = new_persons.sample(n = 281533)
+        random_persons = new_persons.sample(n = round(index/(x+1))) #TODO: Check / Needs round
         random_persons.reset_index(inplace=True)
         random_persons['ID'].head(10)
 
         # Taking random ids from policies to make them match
-        random_policies = new_policies.sample(n = 281533)
+        random_policies = new_policies.sample(n = round(index/(x+1))) #TODO: Check
         random_policies.reset_index(inplace=True)
         random_policies['ID'].head(10)
 
@@ -81,10 +82,9 @@ def combine(times):
         # Add random PERSONS from persons new for matching
         # Add random POLICY ID from policies new for matching 
         with pd.option_context('mode.chained_assignment',None):
-            index = 281533
-            index = index*(x+1) #TODO Check dynamic index if it's working always
+            index = index*(x+1) #TODO Check dynamic index if it's working always /  check number if its really progressive after runs
             print(index, "INDEX")
-            replace_pp = claims_2[index:] # TODO: check number if its really progressive after runs
+            replace_pp = claims_2[index:]
             replace_pp.reset_index(inplace=True)
             replace_pp['PERSON_ID'] = random_persons['ID'].copy()
             replace_pp['POLICY_ID'] = random_policies['ID'].copy()
@@ -98,9 +98,9 @@ def combine(times):
         claims2_new.reset_index(inplace=True)
         claims2_new.drop(["level_0","index"], axis = 1, inplace=True)
         print("Claims dataset enlarged and matched", claims2_new)
-        claims2_new.to_csv(("/Users/ludovicocesaro/Downloads/test/{}/claims.csv").format(times))
-        persons_2.to_csv(("/Users/ludovicocesaro/Downloads/test/{}/persons.csv").format(times))
-        policies_2.to_csv(("/Users/ludovicocesaro/Downloads/test/{}/policies.csv").format(times))
+        # claims2_new.to_csv(("/Users/ludovicocesaro/Downloads/test/{}/claims.csv").format(times))
+        # persons_2.to_csv(("/Users/ludovicocesaro/Downloads/test/{}/persons.csv").format(times))
+        # policies_2.to_csv(("/Users/ludovicocesaro/Downloads/test/{}/policies.csv").format(times))
         print("Combination of the anonymized dataset completed", x+1, "run(s) of ", times)
     print('ok')
 generate_anonymized_data()
