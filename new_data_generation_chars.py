38fba5b0e0a28a7402fe54582c8dfcc97e57fc64
lcesaro-adh <ludovico.cesaro@extern-azdigital-health.com>
2022-05-05 14:40:30 +0300

    small improvements, looks good, index saved csv to be fixed

--- new_data_generation_chars.py
+++ new_data_generation_chars.py
@@ -1,28 +1,30 @@
 import pandas as pd
-
 pd.set_option("display.max_columns", None)
-
 
 def generate_anonymized_data():
     """
-        Function that generates more anonymized data starting from a base ridm file already anonymized adding characters 'ABC' then combine it
+    Function that generates more anonymized data starting from a base ridm file already anonymized adding characters 'ABC' then combine it
     for times defined
     """
     times = int(
         input(
-            "Please enter how many times you want to enlarge the datasets: (exponential)\n"
+            "Please enter how many times you want to enlarge exponentially the initial datasets\n"
         )
     )
     print(f"The enlargment will be compounded by {times} times")
 
-    policies = pd.read_csv("/Users/ludovicocesaro/Downloads/test/0/policies.csv")
     claims = pd.read_csv("/Users/ludovicocesaro/Downloads/test/0/claims.csv")
     persons = pd.read_csv("/Users/ludovicocesaro/Downloads/test/0/persons.csv")
+    policies = pd.read_csv("/Users/ludovicocesaro/Downloads/test/0/policies.csv")
+
+    print("Tables before enlargement")
+    print("Size claims:", claims.memory_usage().sum() / 1000000, "Mb")
+    print("Size persons:", persons.memory_usage().sum() / 1000000, "Mb")
+    print("Size policies:", policies.memory_usage().sum() / 1000000, "Mb")
 
     for x in range(times):
-
-        index = 281533
-        # Creating new claims, persons , policies primary keys
+        index = len(claims.index)
+        # Creating new claims, persons , policies + dummy primary keys creation
         new_claims = claims.copy()
         new_claims["ID"] = new_claims["ID"] + "ABC"
         new_persons = persons.copy()
@@ -31,13 +33,13 @@ def generate_anonymized_data():
         new_policies["ID"] = new_policies["ID"] + "ABC"
 
         # Taking random ids from persons and policies to make them match with claims foreign keys
-        random_persons = new_persons.sample(n=index * pow(2, x), replace=True)
+        random_persons = new_persons.sample(n=index * pow(2, x), replace=True) # index with pow for compounding
         random_persons.reset_index(inplace=True)
 
         random_policies = new_policies.sample(n=index * pow(2, x), replace=True)
         random_policies.reset_index(inplace=True)
 
-        # Replacing correct keys to new generated claims
+        # Replacing keys to new generated claims
         new_claims["PERSON_ID"] = random_persons["ID"]
         new_claims["POLICY_ID"] = random_policies["ID"]
 
@@ -108,6 +110,7 @@ def generate_anonymized_data():
         "NEW_BUSINESS",
     ]
 
+    # Check columns and fix properly
     new_matched_claims.drop(
         columns=[col for col in new_matched_claims if col not in claims_columns],
         inplace=True,
@@ -121,13 +124,15 @@ def generate_anonymized_data():
         inplace=True,
     )
 
-    fix_size(new_matched_claims, concat_persons, concat_policies)  # correct
+    fix_size(new_matched_claims, concat_persons, concat_policies)
 
-
-# TODO Finish fix size
 def fix_size(claims, persons, policies):
 
-    tables = [{"tableName":'claims', "dataframe":claims},{'tableName':'persons', "dataframe": persons},{'tableName':'policies', "dataframe": policies}]
+    tables = [
+        {"tableName": "claims", "dataframe": claims},
+        {"tableName": "persons", "dataframe": persons},
+        {"tableName": "policies", "dataframe": policies},
+    ]
     byte = 1000000
 
     print("Tables after enlargement")
@@ -138,32 +143,33 @@ def fix_size(claims, persons, policies):
     answer = input("Do you accept the current sizes? y/n: ")
 
     if answer == "y":
-        #TODO: Save
+        print(claims, 'CLAIMS')
+        claims.to_csv("/Users/ludovicocesaro/Downloads/test/0/claims.csv")
+        persons.to_csv("/Users/ludovicocesaro/Downloads/test/0/persons.csv")
+        policies.to_csv("/Users/ludovicocesaro/Downloads/test/0/policies.csv")
+        print('Tables correcly saved.')
         raise SystemExit
 
     else:
         decrease = int(
-            input("How much do you want to decrease the size in percentage?")
+            input("How much do you want to decrease the size in percentage? ")
         )
         for data in tables:
-            print(data["tableName"])
             total = len(data["dataframe"].index)
             toremove = round(total * decrease / 100)
             last = total - toremove
             final_decreased = data["dataframe"][:last]
-            # FORMAT DOES NOT WORK, MUST HAVE TABLE NAME NOT DATAFRAME
-            #name = data.tablename
-            print(data, 'data')
-
+            print(final_decreased, 'FINAL')
             final_decreased.to_csv(
-                ("/Users/ludovicocesaro/Downloads/test/0/{}.csv").format(data["tableName"])
+                ("/Users/ludovicocesaro/Downloads/test/0/{}.csv").format(
+                    data["tableName"]
+                )
             )
             print(
                 data["tableName"],
                 "reduced and saved. Current size",
-                data["dataframe"].memory_usage().sum() / byte,
+                data["dataframe"][:last].memory_usage().sum() / byte,
                 "Mb",
             )
-
 
 generate_anonymized_data()
