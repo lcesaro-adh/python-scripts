2cbea471f2ccb057d3290f598b49b340c2ae0a48
lcesaro-adh <ludovico.cesaro@extern-azdigital-health.com>
2022-05-12 11:32:19 +0300

    working for more columns, saving needs to be added

--- test_data_generation/test_data_generation.py
+++ test_data_generation/test_data_generation.py
@@ -2,44 +2,32 @@ import pandas as pd
 pd.set_option("display.max_columns", None)
 pd.options.mode.chained_assignment = None
 
-sourcefiles = ["claims.csv","policies.csv", "persons.csv"]
-    
-keys = ["ID", "PERSON_ID", "POLICY_ID"]
-
 path = "/Users/ludovicocesaro/Desktop/Files/Reply/Allianz/Other/Scripts/test_data/test_data_generation/input/"
+sourcefiles = ["claims.csv","policies.csv", "persons.csv", "personPolicyAssociations.csv", "diagnosis.csv", "providers.csv"]
+keys = ["ID", "PERSON_ID", "POLICY_ID", "CLAIM_ID", "PROVIDER_ID"]
 
 def readRidm(path, sourcefiles): # Read ridm and save as dictionary
     print("start read ridm from path: " + path)
     read_files = {}
     for filename in sourcefiles:
-        read_files[filename.split(".")[0]] = pd.read_csv(path + filename, dtype=object, index_col=[0])
+        read_files[filename.split(".")[0]] = pd.read_csv(path + filename, dtype=object)
         print(filename)
     print(str(len(sourcefiles)) + " files loaded")
     return read_files
 
 def double_df(df, list_columns): # Enlarge only creating keys for the list_columns
-        df_copy = df.copy()
-        for column in list_columns:
-            for real_column in df.columns:
-                if real_column==column:
-                    print('enlarging')
-                    df_copy[column] = df_copy[column] + "ABC"
-        df = pd.concat([df, df_copy])
-        df.reset_index(inplace=True)
-        df.drop(['index'], axis=1, inplace=True)
-        print(df, 'df')
-
-def match_f_keys(df, list_columns):
+    df_copy = df.copy()
     for column in list_columns:
-        for real in df.columns:
-            if real==column:
-                print('enlarging')
-                # df_copy = df.copy()
-                # df_copy[column] = df_copy[column] + "ABC"
-                # df = pd.concat([df, df_copy])
-                # df.reset_index(inplace=True)
-                # df.drop(['index'], axis=1, inplace=True)
-                # print(df, 'df')
+            for real_column in df.columns:
+                print(real_column, column)
+                if real_column==column:
+                    df_copy[column] = df_copy[column] + "ABC"
+    df = pd.concat([df, df_copy])
+    df.reset_index(inplace=True, drop=True)
+    print(df, 'df')
+
+def decrease_tablesize():
+    print('decrease')
 
 # Read all tables
 tableread = readRidm(path, sourcefiles)
