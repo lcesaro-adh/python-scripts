0554a0540ba951ca7316069ddd3fbb73374bdcbb
lcesaro-adh <ludovico.cesaro@extern-azdigital-health.com>
2022-05-12 17:33:15 +0300

    update

--- test_data_generation/test_data_generation.py
+++ test_data_generation/test_data_generation.py
@@ -4,13 +4,7 @@ pd.options.mode.chained_assignment = None
 byte = 1000000
 
 path = "/Users/ludovicocesaro/Desktop/Files/Reply/Allianz/Other/Scripts/test_data/test_data_generation/input/"
-sourcefiles = ["providers.csv", "diagnosis.csv", "treatments.csv", "claims.csv",
-                    "policies.csv", "persons.csv", "personPolicyAssociations.csv",
-                    "subCoverages.csv", "contracts.csv", "operationalEntities.csv", "businessRelations.csv", "products.csv",
-                    "exchangeRates.csv", "clients.csv", "clientEntities.csv", "certificates.csv", "retailClients.csv", "cashflows.csv", "territories.csv",
-                    "uwTerritoryAssociations.csv", "borderauxItems.csv", "businessDivisions.csv", "productSubCovAssociations.csv", "costContainmentProducts.csv",
-                    "costContainmentBundles.csv", "costContainmentSubCoverages.csv"
-                    ]
+sourcefiles = ["claims.csv","policies.csv", "persons.csv"]
 keys = ["ID", "PERSON_ID", "POLICY_ID", "CLAIM_ID", "PROVIDER_ID", "OE_ID", "BUSINESS_DIVISION_ID", "CLIENT_ID", "BUSINESS_PARTNER_ID", "BORDERAUX_ITEM_ID", "BUSINESS_RELATION_ID","CERTIFICATE_ID", "CONTRACT_ID", "LOCAL_POLICY_ID", "PRODUCT_ID", "LOCAL_PRODUCT_ID", "TERRITORY_ID", "COST_CONTAINMENT_BUNDLE_ID", "COST_CONTAINMENT_SUB_COVERAGE_ID", "SUB_COVERAGE_ID", "LOCAL_CLAIM_ID"]
 
 def readRidm(path, sourcefiles): # Read ridm and save as dictionary
@@ -32,31 +26,44 @@ def double_df(df, list_columns, table): # Enlarge only creating keys for the lis
     df.reset_index(inplace=True, drop=True)
     print(df)
     return df
-    #df.to_csv(("test_data_generation/output/{}.csv").format(table))
 
-# def decrease_tablesize(df, table):
-#     answer = input("Do you accept the current sizes? y/n: ")
-#     if answer == "n":
-#         decrease = int(
-#         input("How much do you want to decrease the size in percentage? "))
-#         total = len(df.index)
-#         toremove = round(total * decrease / 100)
-#         last = total - toremove
-#         data_decreased = df[:last]
-#         print(
-#             table,
-#             "reduced. Current size",
-#             data_decreased.memory_usage().sum() / byte,
-#             "Mb",
-#         )
+def ask_decrease():
+    answer = input("Do you accept the current sizes? y/n: ")
+    if answer == "n":
+        decrease = int(
+        input("How much do you want to decrease the size in percentage? "))
+    return decrease
 
+def decrease_tablesize(df, decrease):
+    total = len(df.index)
+    print(df.info())
+    toremove = round(total * decrease / 100)
+    last = total - toremove
+    data_decreased = df[:last]
+    print(
+        "reduced. Current size",
+        data_decreased.memory_usage().sum() / byte,
+        "Mb",
+    )
+    #data_decreased.to_csv(("test_data_generation/output/{}.csv").format(table))
+        
 # Read all tables
 tableread = readRidm(path, sourcefiles)
 
-for table in tableread:  # For these tables
-    print('running',table)
-    df = double_df(tableread[table],keys, table)
-    # decrease_tablesize(df, table)
+def round_table(tr):
+    df = None
+    for table in tr:  # For these tables
+        print('running',table)
+        print(keys)
+        df = double_df(tableread[table],keys, table)
+    return df
 
+decrease = ask_decrease()
+
+df = round_table(tableread)
+print(df)
+
+print(df, 'decrease table')
+decrease_tablesize(df, decrease)
     # Asks if the size are okay
     # If not okay decrease by percentage
