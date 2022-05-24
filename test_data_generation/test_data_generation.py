da3b7ed34342b736fe58df5e05327777181aa2ac
lcesaro-adh <ludovico.cesaro@extern-azdigital-health.com>
2022-05-24 20:17:47 +0300

    duckup in master

--- test_data_generation/test_data_generation.py
+++ test_data_generation/test_data_generation.py
@@ -1,11 +1,20 @@
 import pandas as pd
+import os
+import logging
 
 pd.set_option("display.max_columns", None)
 pd.options.mode.chained_assignment = None
-byte = 1000000
+BYTE = 1000000
 
-path = "/Users/ludovicocesaro/Desktop/Files/Reply/Allianz/Other/Scripts/test_data/test_data_generation/rids/"
-sourcefiles = ["claims.csv", "policies.csv", "persons.csv"]
+custom_path = os.getcwd()
+path = custom_path + "/test_data_generation/ridm/"
+sourcefiles = ["providers.csv", "diagnosis.csv", "treatments.csv", "claims.csv",
+                    "policies.csv", "persons.csv", "personPolicyAssociations.csv",
+                    "subCoverages.csv", "contracts.csv", "operationalEntities.csv", "businessRelations.csv", "products.csv",
+                    "exchangeRates.csv", "clients.csv", "clientEntities.csv", "certificates.csv", "retailClients.csv", "cashflows.csv", "territories.csv",
+                    "uwTerritoryAssociations.csv", "borderauxItems.csv", "businessDivisions.csv", "productSubCovAssociations.csv", "costContainmentProducts.csv",
+                    "costContainmentBundles.csv", "costContainmentSubCoverages.csv"
+                    ]
 keys = [
     "ID",
     "PERSON_ID",
@@ -31,17 +40,21 @@ keys = [
 ]
 
 
-def readRidm(path, sourcefiles):  # Read ridm and save as dictionary
-    print("start read ridm from path: " + path)
+def read_ridm(path, sourcefiles):
+    """Read ridm and save as dictionary"""
+    logging.warning("start read ridm from path: " + path)
     read_files = {}
     for filename in sourcefiles:
         read_files[filename.split(".")[0]] = pd.read_csv(path + filename, dtype=object)
-        print(filename)
-    print(str(len(sourcefiles)) + " files loaded")
+        logging.warning(filename)
+    logging.warning(str(len(sourcefiles)) + " files loaded")
     return read_files
 
 
-def double_df(df, list_columns):  # Enlarge df creating keys for the list_columns
+def double_df(df, list_columns):
+    """Enlarge df creating keys for the list_columns
+    df -> type: Dataframe
+    list_columns -> type: List"""
     df_copy = df.copy()
     for column in list_columns:
         for real_column in df.columns:
@@ -51,44 +64,65 @@ def double_df(df, list_columns):  # Enlarge df creating keys for the list_column
     return df
 
 
-def ask_decrease():
-    print("Enter 0 to increase the size, any other to decrease")
-    decrease = int(input("How much do you want to decrease the size in percentage? "))
-    return decrease
+def ask_operation():
+    answer = input("Do you want to increase (inc) or decrease (dec) the current size?")
+    return answer
 
 
 def log_size(df, table):
-    print(
-        table,
-        "current size",
-        df.memory_usage().sum() / byte,
-        "Mb",
-    )
+    """Log tablename and size in Mb
+    df -> type: Dataframe
+    table -> type: string
+    """
+    message = (table, "current size", df.memory_usage().sum() / BYTE, "Mb")
+    logging.warning(message)
 
 
-def decrease_tablesize(df, table, decrease):  # Decrease df and save csv
+def increase_tablesize(df, table, increase):
+    """Increase df and save csv
+    df -> type: Dataframe
+    table -> type: string
+    increase -> type: int"""
+    for i in range(increase):
+        if i == 0:
+            df = tablesread[table]
+        df = double_df(df, keys)
+        log_size(df, table)
+        df.to_csv(
+            ("{}/test_data_generation/ridm/{}.csv").format(custom_path, table),
+            index=False,
+        )
+
+
+def decrease_tablesize(df, table, decrease):
+    """Decrease df and save csv
+    df -> type: Dataframe
+    table -> type: string
+    decrease -> type: int
+    """
     total = len(df.index)
     toremove = round(total * decrease / 100)
     last = total - toremove
     data_decreased = df[:last]
     log_size(data_decreased, table)
-    print("Saving...")
     data_decreased.to_csv(
-        ("test_data_generation/rids/{}.csv").format(table), index=False
+        ("{}/test_data_generation/ridm/{}.csv").format(custom_path, table), index=False
     )
 
-#----
-# Read all tables
-tableread = readRidm(path, sourcefiles)
+if __name__ == "__main__":
+    tablesread = read_ridm(path, sourcefiles)
 
-# Log size of initial tables
-for table in tableread:
-    log_size(tableread[table], table)
+    for table in tablesread:
+        log_size(tablesread[table], table)
 
-# asks decrease after telling how big is the table
-decrease = ask_decrease()  # ask decrease before because of logic issue
-
-# Double content of tables
-for table in tableread:
-    df = double_df(tableread[table], keys)
-    decrease_tablesize(df, table, decrease)
+    answer = ask_operation()
+    if answer == "inc":
+        increase = int(input("How many times you want to increase?"))
+        for table in tablesread:
+            increase_tablesize(tablesread[table], table, increase)
+    else:
+        decrease = int(
+            input("How much do you want to decrease the sizes? (in percentage)")
+        )
+        for table in tablesread:
+            decrease_tablesize(tablesread[table], table, decrease)
