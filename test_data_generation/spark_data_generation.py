bd02f6be6061f6b0e34e9752117b56db98fcc9eb
lcesaro-adh <ludovico.cesaro@extern-azdigital-health.com>
2022-06-27 21:35:10 +0300

    finale dei finali

--- test_data_generation/spark_data_generation.py
+++ test_data_generation/spark_data_generation.py
@@ -1,14 +1,15 @@
 import string
-from pyspark.sql import SparkSession
-import pyspark.sql.functions as F
-from pyspark.sql import DataFrame as SparkDataFrame
-from pyspark.sql.functions import desc
+from typing import List
 import click
+import pyspark.sql.functions as F
 from pyspark import SparkContext
-from tasks.logs.logger import Logger
+from pyspark.sql import DataFrame as SparkDataFrame
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import concat, desc, lit
 from tasks.common.pyspark.loading import load_dataset
 from tasks.common.pyspark.saving import save_dataset
 from tasks.common.ridm.ridm_spec_processing import get_ridm_spec
+from tasks.logs.logger import Logger
 
 DEFAULT_RIDM_VERSION = "v1"
 logger = Logger()
@@ -22,7 +23,7 @@ logger = Logger()
     help="Input folder with the base ridm to start",
 )
 @click.option(
-    "--output", "-o", required=True, type=str, help="Location folder where to output (should be the same as input)"
+    "--output", "-o", required=True, type=str, help="Location folder where to output"
 )
 @click.option(
     "--action", "-a", required=True, type=str, help="Action you want to do inc/dec"
@@ -34,112 +35,145 @@ logger = Logger()
     type=int,
     help="Quantity enlargement(times)/decrease(percentage)",
 )
+@click.option(
+    "--test",
+    "-t",
+    is_flag=True,
+    default=False,
+    help="Set flag to true to run test case.",
+)
 @click.option("--context", "-c", default="slamApp", type=str, help="Spark context name")
-def main(input: str, output: str, action: str, amount: int, context: str) -> None:
+def main(
+    input: str, output: str, action: str, amount: int, test: bool, context: str
+) -> None:
     spark = SparkSession.builder.appName(context).getOrCreate()
-    path = input
-    output_path = output
-    sourcefiles = ["claims"]#, "policies", "persons"] # temporarily removed to make test works
+
+    if test:
+        sourcefiles = ["claims"]
+    else:
+        sourcefiles = [
+            "providers",
+            "diagnosis",
+            "treatments",
+            "claims",
+            "policies",
+            "persons",
+            "personPolicyAssociations",
+            "subCoverages",
+            "contracts",
+            "operationalEntities",
+            "businessRelations",
+            "products",
+            "exchangeRates",
+            "clients",
+            "clientEntities",
+            "certificates",
+            "retailClients",
+            "cashflows",
+            "territories",
+            "uwTerritoryAssociations",
+            "borderauxItems",
+            "businessDivisions",
+            "productSubCovAssociations",
+            "costContainmentProducts",
+            "costContainmentBundles",
+            "costContainmentSubCoverages",
+        ]
 
     ridm_relations = get_ridm_spec(DEFAULT_RIDM_VERSION)
-
     keys_list = []
     for a in ridm_relations[1]:
-        key = ridm_relations[1][a]['right_field']
+        key = ridm_relations[1][a]["right_field"]
         keys_list.append(key)
-    keys_list.append("ID")  # force append ID because key missing
-    print(keys_list)
+    keys_list.append("ID")  # force append ID because ID key missing
+    keys_list = list(dict.fromkeys(keys_list))  # removing double keys
 
-    tablesread = read_ridm(input, path, sourcefiles, spark)
+    tables_read = read_ridm(input, output, sourcefiles, spark)
 
     if action == "inc":
-        for table in tablesread:
-            increase_tablesize(tablesread[table], table, amount, tablesread, keys_list, output_path)
+        for table in tables_read:
+            increase_tablesize(
+                tables_read[table], table, amount, tables_read, keys_list, output
+            )
     else:
-        for table in tablesread:
-            decrease_tablesize(tablesread[table], table, amount, output_path)
+        for table in tables_read:
+            decrease_tablesize(tables_read[table], table, amount, output)
+
 
 def read_ridm(input: str, path: str, sourcefiles: dict, spark: SparkContext):
     """Read ridm and save as dictionary"""
-    # logger._logger_technical.info("start reading ridm from path:"+path)
-    print("start reading ridm from part:" + path)
+    logger._logger_technical.info("start reading ridm from path:"+path)
     input_tables = {
         input: sourcefiles,
     }
     read_files = {}
     for filename in sourcefiles:
         read_files = load_dataset(input_tables, spark, "<NA>")
-        #logger._logger_technical.info(filename)
-        print(filename)
-    #logger._logger_technical.info(str(len(sourcefiles))+"files loaded")
-    print(str(len(sourcefiles)) + " files loaded")
+        logger._logger_technical.info(filename)
+    logger._logger_technical.info(str(len(sourcefiles))+"files loaded")
     return read_files
 
-def double_df(df: SparkDataFrame, list_columns: list):
-    """Enlarge df creating keys for the list_columns"""
+
+def double_df(df: SparkDataFrame, list_columns: List[str]):
+    """Enlarge the size of the df creating new keys for the matching"""
     df_copy = df.select("*")
     for column in list_columns:
         for real_column in df.columns:
             if real_column == column:
-
-                #df_copy[column] = df_copy[column] + "_A"
-                df_copy = df_copy.withColumn(  # !!- Matching does not work fine
-                    column,
-                    F.concat(
-                        F.expr(f"substring({column}, 0, length({column}))"),
-                        F.lit("_A"),
-                        F.substring(f"{column}", 0, 0),
-                    ),
-                )
-
-               #df_copy[column] = df_copy.select(concat(col(column), lit("_A"))).withColumnRenamed(f"concat({column},_A)", column)
-               # above line not working -> TypeError: 'DataFrame' object does not support item assignment
-
+                add_char = [column, lit("_A")]
+                df_copy = df_copy.withColumn(column, concat(*add_char))
     df = df.union(df_copy)
     return df
 
-def increase_tablesize(df: SparkDataFrame, table: str, increase: int, tablesread: dict, keys_list: list, output_path: str):
-    """Increase df and save csv in the respective folder"""
+
+def increase_tablesize(
+    df: SparkDataFrame,
+    table: str,
+    increase: int,
+    tablesread: dict,
+    keys_list: list,
+    output: str,
+):
+    """Increase the size of the df and saves its csv in the output folder"""
     for i in range(increase):
         if i == 0:
             df = tablesread[table]
         df = double_df(df, keys_list)
-        df = df.sort(desc("ID"))
-
+        if "ID".upper() in (name.upper() for name in df.columns):
+            df = df.sort(desc("ID"))
         table_dict = {table: df}
-        mod_path = output_path + table
+        mod_path = output + table
         save_dataset(table_dict, mod_path, "")
+        logger._logger_technical.info(f"{table}increased and saved correctly")
 
-        print("increased and saved correctly")
-        # logger._logger_technical.info(f"{table}increased and saved correctly")
 
-def decrease_tablesize(df: SparkDataFrame, table: string, decrease: int, output_path: str):
-    """Decrease df and save csv in its folder"""
+def decrease_tablesize(
+    df: SparkDataFrame, table: string, decrease: int, output: str
+):
+    """Decrease the size of the df by percentage and saves its csv in the output folder"""
     total = df.count()
-    toremove = round(total * decrease / 100)
-    last = total - toremove
+    to_remove = round(total * decrease / 100)
+    last = total - to_remove
     data_decreased = df.withColumn("index", F.monotonically_increasing_id())
     data_decreased = data_decreased.sort("index").limit(last)
-    data_decreased = data_decreased.sort(desc("ID"))
-
+    if "ID" in (name.upper() for name in df.columns):
+        data_decreased = data_decreased.sort(desc("ID"))
     table_dict = {table: data_decreased}
-    mod_path = output_path + table
+    mod_path = output + table
     save_dataset(table_dict, mod_path, "")
-
-    print("decreased and saved correctly")
+    logger._logger_technical.info(f"{table}decreased and saved correctly")
 
 
 if __name__ == "__main__":
     """
-    Script to generate/reduce RIDMs size
+    Script to generate/reduce RIDMs size with the matchings
     Given the path to a directory containing a defined subset of RIDM tables,
-    the operation wanted to perform and the amount it enlarges/reduces the
-    content of the folder and saves it
+    an output folder, the operation and the amount wanted it enlarges/reduces the input folder and then saves it
 
-    Example increment:
-    python data_generation.py -input /ridm/ -action inc -amount 1
+    Example increment by 1 time :
+    python data_generation.py -input /ridm/ -output /output_ridm/ -action inc -amount 1
 
-    Example decrement:
-    python data_generation.py -input /ridm/ -action dec -amount 50
+    Example decrement by 50% :
+    python data_generation.py -input /ridm/ -output /output_ridm/ -action dec -amount 50
     """
     main()
