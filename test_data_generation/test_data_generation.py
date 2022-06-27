5d3b1e8f2118a736a7dc5fd745474b51f96a0a0e
lcesaro-adh <ludovico.cesaro@extern-azdigital-health.com>
2022-06-27 12:40:53 +0300

    path generalized

--- test_data_generation/test_data_generation.py
+++ test_data_generation/test_data_generation.py
@@ -1,8 +1,7 @@
 import os
 from tempfile import TemporaryDirectory
-
 import pandas as pd
-from pyspark.sql import Column, DataFrame, SparkSession
+from pyspark.sql import SparkSession
 
 from tasks.common.utilities.io import (
     get_directory_path_from_file_path,
@@ -12,24 +11,17 @@ from tasks.logs.logger import Logger
 
 logger = Logger()
 
-SRC_DIRECTORY = f"{get_directory_path_x_levels_up_from_file_path(__file__, 5)}Allianz/Projects/adhdh-semantic-layer-aggregate-model/test_data_generation/"
+SRC_DIRECTORY = f"{get_directory_path_x_levels_up_from_file_path(__file__, 2)}/test_data_generation/"
 _THIS_DIRECTORY_PATH = get_directory_path_from_file_path(__file__)
-
-# For the test I need
-# src directory with very small amount of data ( 1 line is okay )
-# expected output with the command already ran with the data
 
 def test_generate(spark_session: SparkSession) -> None:
     # Arrange
     with TemporaryDirectory() as temp_dir:
         command = (
-            f" python {SRC_DIRECTORY}data_generation.py -i /Users/ludovicocesaro/Desktop/Files/Reply/Allianz/Projects/adhdh-semantic-layer-aggregate-model/tests/test_data_generation/start_test_ridm/ -o {temp_dir}/ -a inc -am 1"
+            f" python {SRC_DIRECTORY}data_generation.py -i {_THIS_DIRECTORY_PATH}start_test_ridm/ -o {temp_dir}/ -a inc -am 1"
         )
-        print(temp_dir, "TEMP_DIR")
         expected_output_file_path = f"{_THIS_DIRECTORY_PATH}ridm/claims.csv"
-        print(expected_output_file_path, "EXP_OUT")
         expected_output_df = pd.read_csv(expected_output_file_path)
-
         # Act
         os.system(command)
         # Assert
